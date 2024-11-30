dea9c0ee31164d016415494c8706249c77ab9273
Happiness Maina <mainahappiness361@gmail.com>
2024-11-30 12:46:02 +0300

    speed.js

--- speed.js
+++ speed.js
@@ -1,17 +1,21 @@
 function speedDetector(){
     const speedLimit=70;
 
-    // prompt speed
-    let speed=prompt("Enter Speed")
+    // prompt user to enter speed
+    let speed = parseFloat(prompt("Enter the speed of the car "));
+
+    
 
 //check speed
     if (speed<speedLimit){
         console.log("OK")
     }else if (speed>speedLimit){
         //calculate demerit points
-        const demeritPoints=Math.floor((speed-speedLimit/5))
+        const demeritPoints=Math.floor((speed-speedLimit)/5)
         console.log("Demerit points" + demeritPoints)
+        // check if license should be suspended
     }else if (demeritPoints>12){
         console.log("License suspended")
     }
 }
+speedDetector()
